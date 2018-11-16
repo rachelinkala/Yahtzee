3a31039d240a965361f9899e8c17a543646d7bef
Rachel Inkala <rachelinkala@gmail.com>
2018-11-15 18:09:14 -0600

    update gemfile

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,32 @@
-import React from 'react';
-import Game from './components/Game';
+import React from 'react'
+import Game from './components/Game'
 import { Route, Switch } from 'react-router-dom'
 import { loginUser, register, logout } from './reducers/user'
-import { ProtectedRoute, Login, Register, NavBar } from '@devpoint/dps-react-kit'
-import FetchUser from './FetchUser'
+import { 
+  ProtectedRoute,
+  Login,
+  Register,
+  NavBar,
+} from '@devpoint/dps-react-kit'
+import FetchUser from './components/FetchUser'
+import Scores from './components/Scores'
+
+const links = [
+    { text: 'Scores', url: '/scores' }
+]
 
 
 const App = () => (
   <>
-    <NavBar handleLogout={logout} />
+    <NavBar handleLogout={logout} authRoutes={links}/>
     <FetchUser>
       <Switch>
         <ProtectedRoute exact path="/" component={Game} />
+        <ProtectedRoute exact path="/scores" component={Scores} />
         <Route
           exact
           path="/login"
-          render={ props => <Login {...props} handleLogin={loginUser} /> }
+          render={ props => <Login {...props} handleLogin={loginUser}  /> }
         />
         <Route
           exact
@@ -27,4 +38,5 @@ const App = () => (
   </>
 )
 
-export default App;
+export default App
+
